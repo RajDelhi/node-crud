a46dca9cea298425bff4bca0903d1562bb7725a7
Rajender Kumar <rajendrak@beckett.com>
2023-10-25 21:15:59 +0530

    test

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const express = require('express')
-const path = require("path")
-const bodyparser = require("body-parser")
-const dotenv = require('dotenv').config() //  this will call environment file automatillcaly 
-const app = express()
-const dbConnect = require('./server/database/dbconnection') 
+const express = require("express");
+const path = require("path");
+const bodyparser = require("body-parser");
+const dotenv = require("dotenv").config(); //  this will call environment file automatillcaly
+const app = express();
+const dbConnect = require("./server/database/dbconnection");
 
+const PORT = process.env.PORT || 8080;
+const HOSTNAME = process.env.HOSTNAME || "localhost";
 
-const PORT = process.env.PORT || 8080
-const HOSTNAME = process.env.HOSTNAME || 'localhost'
-
-app.set("view engine","ejs")
-app.set("views")
+app.set("view engine", "ejs");
+app.set("views");
 
 // Using express.urlencoded middleware
 // app.use(express.urlencoded({
@@ -18,31 +17,25 @@ app.set("views")
 // }))
 
 // Body-parser middleware
-app.use(bodyparser.urlencoded({ extended: true }))
-app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 
- const base_path = __dirname;
+const base_path = __dirname;
 // app.set("views",base_path+"/views/module/grading")// default path
 // app.set("views".path.resove(base_path,"views/module.grading"))
- 
 
 // Creating a virtual path of assets folder for future use, using middle ware
-app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
-app.use('/img', express.static(path.resolve(__dirname, "assets/img")))
-app.use('/js', express.static(path.resolve(__dirname, "assets/js")))
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
 dbConnect();
 
-
-// Load Router 
-const router = require('./server/routes/user-routes')
-app.use('/',router)
+// Load Router
+const router = require("./server/routes/user-routes");
+app.use("/", router);
 // OR app.use('/',require('./server/routes/user-routes'))
- 
-
 
 app.listen(PORT, HOSTNAME, () => {
-    console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
-  }); 
- 
- 
+  console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+});
